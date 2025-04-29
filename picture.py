954c7bc333d42ddcbebc432979db5bad1cdc8144
Dawnerino <26774947+Dawnerino@users.noreply.github.com>
2025-04-28 21:14:36 -0500

    fix pathing issues

--- picture.py
+++ picture.py
@@ -556,19 +556,19 @@ def main_loop():
             global wordiness
             if wordiness == 50:
                 wordiness = 100
-                audio_manager.play_sound("sys_aud/wordiness/small.wav",100)
+                audio_manager.play_sound("sys_aud/wordiness/small.wav")
             elif wordiness == 100:
                 wordiness = 200
-                audio_manager.play_sound("sys_aud/wordiness/normal.wav",100)
+                audio_manager.play_sound("sys_aud/wordiness/normal.wav")
             elif wordiness == 200:
                 wordiness = 500
-                audio_manager.play_sound("sys_aud/wordiness/big.wav",100)
+                audio_manager.play_sound("sys_aud/wordiness/big.wav")
             elif wordiness == 500:
                 wordiness = 1000
-                audio_manager.play_sound("sys_aud/wordiness/largest.wav",100)
+                audio_manager.play_sound("sys_aud/wordiness/largest.wav")
             else:
                 wordiness = 50
-                audio_manager.play_sound("sys_aud/tiny.wav",100)
+                audio_manager.play_sound("sys_aud/wordiness/tiny.wav")
             print(f"Set wordiness to: {wordiness}")
             serialHandle.send_serial_command(f"WORDINESS_{wordiness}")
             
